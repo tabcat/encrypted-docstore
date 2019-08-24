95cc82ea4d7a61b0fcc054567c7cdac6ae7c90ce
anders <andersb@tuta.io>
2019-08-24 14:50:15 -0500

    added genKey method and fixed array buffer deserialization

--- src/encryptedDocstore.js
+++ src/encryptedDocstore.js
@@ -1,19 +1,19 @@
 
 'use strict'
-// probably do something with this emitter later
-const EventEmitter = require('events').EventEmitter
 const bs58 = require('bs58')
 const Buffer = require('safe-buffer').Buffer
 const Key = require('./key')
 
-
-class EncryptedDocstore extends EventEmitter {
+class EncryptedDocstore {
   constructor(encryptedDocstore, key) {
-    super()
-    this.encrypted = encryptedDocstore
-    this.key = key
+    if (!isDefined(encryptedDocstore)) throw new Error('encryptedDocstore must be defined')
+    if (!isDefined(key)) throw new Error('key must be defined')
+    this._docstore = encryptedDocstore
+    this.indexBy = this.docstore.options.indexBy
+    this._key = key
   }
-  // docstore: an instance of orbit docstore with name from determineEncDbName
+
+  // docstore: an instance of orbitdb docstore with name from determineEncDbName
   //  (get this by opening a docstore by address or creating one with a config)
   // key: aes cryptoKey (get this from this.deriveKey)
   static async mount(docstore, key) {
@@ -24,19 +24,17 @@ class EncryptedDocstore extends EventEmitter {
     if (!(await this.keyCheck(docstore.address, key))) {
       throw new Error('keyCheck failed while trying to mount store')
     }
-
     return new EncryptedDocstore(docstore, key)
   }
+
   // use to get name of encDocstore for creating the docstore
-  static async determineEncDbName(orbit, dbConfig, key) {
-    if (orbit === undefined || dbConfig === undefined || key === undefined) {
-      throw new Error('orbit, dbConfig and key must be defined')
+  static async determineEncDbName(orbitdb, dbConfig, key) {
+    if (orbitdb === undefined || dbConfig === undefined || key === undefined) {
+      throw new Error('orbitdb, dbConfig and key must be defined')
     }
-
     const { name, type, options } = dbConfig
-    const root = (await orbit.determineAddress(name, type, options)).root
+    const root = (await orbitdb.determineAddress(name, type, options)).root
     const decodedRoot = bs58.decode(root)
-
     const encRoot = bs58.encode(
       Buffer.from(
         (await key.encrypt(decodedRoot, decodedRoot)).cipherbytes
@@ -45,39 +43,36 @@ class EncryptedDocstore extends EventEmitter {
 
     return `${encRoot}/${root}`
   }
+
   // use to determine address of encDocstore
-  static async determineEncDbAddress(orbit, dbConfig, key) {
-    if (orbit === undefined || dbConfig === undefined || key === undefined) {
-      throw new Error('orbit, dbConfig and key must be defined')
+  static async determineEncDbAddress(orbitdb, dbConfig, key) {
+    if (orbitdb === undefined || dbConfig === undefined || key === undefined) {
+      throw new Error('orbitdb, dbConfig and key must be defined')
     }
     if (dbConfig.type !== 'docstore') {
       throw new Error('dbConfig type must be docstore')
     }
-
-    const encDbName = await this.determineEncDbName(orbit, dbConfig, key)
-
-    return await orbit.determineAddress(encDbName, dbConfig.type, dbConfig.options)
+    const encDbName = await this.determineEncDbName(orbitdb, dbConfig, key)
+    return await orbitdb.determineAddress(encDbName, dbConfig.type, dbConfig.options)
   }
+
   static async keyCheck(address, key) {
     if (address === undefined || key === undefined) {
       throw new Error('address and key must be defined')
     }
-
     const [ encRoot, root ] = address.path.split('/')
     const decodedEncRoot = bs58.decode(encRoot)
-
     // d for deterministic
     // probably the weakest part of the encryptedDocstore to precomputed tables.
     // weakness is related to there being basically no salt so attacker needs
     //  a table with an entry of every encrypted original root from encrypting every
     //  every likely original root with every possible aes-gcm key to know your key?
-    // seems like a lot but targeting specific orbit stores would require only
+    // seems like a lot but targeting specific orbitdb stores would require only
     //  finding every encrypted original root by encrypting one orignal root with
     //  every possible aes-gcm key (if the amateur cryptographer in me is correct)
     // hopefully i am correct in thinking this is adequate security for now
     //  if the bytes deriving the aes key are random enough
     const dIv = bs58.decode(root)
-
     try {
       const decrypted = await key.decrypt(decodedEncRoot, dIv)
       return bs58.encode(Buffer.from(decrypted)) === root
@@ -90,6 +85,9 @@ class EncryptedDocstore extends EventEmitter {
 // cryptographic opterations imported from ./key.js
   // creates an AES key that can be used for an encryptedDocstore
   // require bytes source to have sufficient entropy when implementing
+  static async genKey(...params) {
+    return await Key.genKey(...params)
+  }
   static async deriveKey(...params) {
     return await Key.deriveKey(...params)
   }
@@ -103,7 +101,7 @@ class EncryptedDocstore extends EventEmitter {
 // helpers
   async decryptRecords(encryptedRecords) {
     return await Promise.all(
-      encryptedRecords.map(encDoc => this.key.decryptMsg(encDoc))
+      encryptedRecords.map(encDoc => this._key.decryptMsg(encDoc))
     )
   }
 
@@ -112,10 +110,7 @@ class EncryptedDocstore extends EventEmitter {
     if (indexKey === undefined) {
       throw new Error('indexKey is undefined')
     }
-
-    const indexBy = this.encrypted.options.indexBy
-
-    // code taken from orbit document store get method:
+    // code taken from orbitdb document store get method:
     // https://github.com/orbitdb/orbit-db-docstore/blob/master/src/DocumentStore.js
     indexKey = indexKey.toString()
     const terms = indexKey.split(' ')
@@ -130,13 +125,10 @@ class EncryptedDocstore extends EventEmitter {
       }
       return e.toLowerCase().indexOf(indexKey) !== -1
     }
-
     const filter = caseSensitive
-      ? (i) => i[indexBy].indexOf(indexKey) !== -1
-      : (i) => search(i[indexBy])
-
-    const records = await this.decryptRecords(this.encrypted.query(() => true))
-
+      ? (i) => i[this.indexBy].indexOf(indexKey) !== -1
+      : (i) => search(i[this.indexBy])
+    const records = await this.decryptRecords(this._docstore.query(() => true))
     return await Promise.all(records.map(res => res.internal).filter(filter))
   }
 
@@ -144,29 +136,21 @@ class EncryptedDocstore extends EventEmitter {
     if (typeof doc !== 'object') {
       throw new Error('doc must have type of object')
     }
-
-    const indexBy = this.encrypted.options.indexBy
-    if (!doc[this.encrypted.options.indexBy]) {
-      throw new Error(`doc requires an ${indexBy} field`)
+    if (!doc[this.indexBy]) {
+      throw new Error(`doc requires an ${this.indexBy} field`)
     }
-
     // since real _id is encapsulated in cipherbytes field and external _id is
     // random, we must delete the old entry by querying for the same id
-    try { await this.del(doc[indexBy]) } catch(e) {}
-
-    return await this.encrypted.put(await this.key.encryptMsg(doc))
+    try { await this.del(doc[this.indexBy]) } catch(e) {}
+    return await this._docstore.put(await this._key.encryptMsg(doc))
   }
 
   async del(indexKey) {
     if (indexKey === undefined) {
       throw new Error('indexKey must be defined')
     }
-
-    const indexBy = this.encrypted.options.indexBy
-    const records = await this.decryptRecords(this.encrypted.query(() => true))
-
-    const matches = records.filter(res => res.internal[indexBy] === indexKey)
-
+    const records = await this.decryptRecords(this._docstore.query(() => true))
+    const matches = records.filter(res => res.internal[this.indexBy] === indexKey)
     // if a deletion fails this will clean it up old records
     if (matches.length > 1) {
       console.error(`there was more than one entry with internal key ${indexKey}`)
@@ -174,10 +158,9 @@ class EncryptedDocstore extends EventEmitter {
     if (matches.length === 0) {
       throw new Error(`No entry with key '${indexKey}' in the database`)
     }
-
     // only return first deletion to keep same api as docstore
     return Promise.all(
-      matches.map(res => this.encrypted.del(res.external[indexBy]))
+      matches.map(res => this._docstore.del(res.external[this.indexBy]))
     ).then(arr => arr[0])
   }
 
@@ -185,25 +168,22 @@ class EncryptedDocstore extends EventEmitter {
     if (mapper === undefined) {
       throw new Error('mapper was undefined')
     }
-
     const fullOp = options.fullOp || false
     const decryptFullOp = async(entry) => ({
       ...entry,
       payload: {
         ...entry.payload,
-        value:await this.key.decryptMsg(entry.payload.value).then(res => res.internal),
+        value:await this._key.decryptMsg(entry.payload.value).then(res => res.internal),
       },
     })
-
-    const index = this.encrypted._index
+    const index = this._docstore._index
     const indexGet = fullOp
       ? async(_id) => decryptFullOp(index._index[_id])
       : async(_id) => index._index[_id]
-        ? await this.key.decryptMsg(index._index[_id].payload.value)
+        ? await this._key.decryptMsg(index._index[_id].payload.value)
           .then(res => res.internal)
         : null
     const indexKeys = Object.keys(index._index)
-
     return Promise.all(indexKeys.map(key => indexGet(key)))
       .then(arr => arr.filter(mapper))
   }
--- src/key.js
+++ src/key.js
@@ -13,10 +13,12 @@ function str2ab(str) {
   return buf;
 }
 
-const randomBytes = async (bytesLength) => await crypto.getRandomValues(new Uint8Array(bytesLength))
+const randomBytes = async (bytesLength) =>
+  await crypto.getRandomValues(new Uint8Array(bytesLength))
 
 const encDocFields = ['_id', 'ciphertext', 'iv']
 // returns a new object with only fields that will be encrypted
+
 const cipherSect = (doc) => Object.keys(doc)
   .reduce((accu, current) => !encDocFields.includes(current)
     ? { ...accu, [current]:doc[current] }
@@ -28,11 +30,20 @@ class Key {
   constructor(cryptoKey) {
     this.key = cryptoKey
   }
-  static async deriveKey(bytes, salt, length, purpose) {
+
+  static async genKey(options = {}) {
+    const { length, purpose, } = options
+    const bytes = await randomBytes(16)
+    const salt = await randomBytes(12)
+    const key = await this.deriveKey(bytes, salt, length, purpose, )
+    return key
+  }
+
+  static async deriveKey(bytes, salt, length = 128, purpose = 'encryptedDocstore') {
     if (bytes === undefined || salt === undefined) {
       throw new Error('bytes and salt must be defined')
     }
-
+    if (typeof purpose !== 'string') throw new Error('purpose must have type string')
     const hkdf = await crypto.subtle.importKey(
       'raw',
       bytes,
@@ -40,48 +51,39 @@ class Key {
       false,
       ["deriveKey"],
     )
-
     const cryptoKey = await crypto.subtle.deriveKey(
       {
         name: 'HKDF',
         hash: { name:'SHA-256' },
         salt,
-        info: str2ab(
-          typeof purpose === 'string'
-          ? purpose
-          : 'encryptedDocstore'
-        ),
+        info: str2ab(purpose),
       },
       hkdf,
-      { name:'AES-GCM', length:length || 128 },
-      true,
+      { name:'AES-GCM', length, },
+      true, // exportable
       ['encrypt', 'decrypt']
     )
-
     return new Key(cryptoKey)
   }
+
   static async exportKey(key) {
     if (key === undefined) {
       throw new Error('key must be defined')
     }
-
-    const rawKey = await crypto.subtle.exportKey('raw', key)
-
-    return rawKey
+    return await crypto.subtle.exportKey('raw', key)
   }
+
   static async importKey(rawKey) {
     if (cryptoKeyAb === undefined) {
       throw new Error('cryptoKeyAb must be defined')
     }
-
     const cryptoKey = await crypto.subtle.importKey(
       'raw',
       rawKey,
       { name:'AES-GCM' },
-      true,
+      true, // exportable
       ['encrypt', 'decrypt']
     )
-
     return new Key(cryptoKey)
   }
 
@@ -89,24 +91,19 @@ class Key {
     if (bytes === undefined) {
       throw new Error('bytes must be defined')
     }
-
     // 12bytes is recommended for GCM for computational efficiencies
     iv = iv || await randomBytes(12)
     const algo = { ...this.key.algorithm, iv }
-
     const cipherbytes = new Uint8Array(
       await crypto.subtle.encrypt(algo, this.key, bytes)
     )
-
     return { cipherbytes, iv }
   }
   async decrypt(bytes, iv) {
     if (bytes === undefined || iv === undefined) {
       throw new Error('bytes and iv must be defined')
     }
-
     const algo = { ...this.key.algorithm, iv }
-
     return await crypto.subtle.decrypt(algo, this.key, bytes)
   }
 
@@ -119,25 +116,21 @@ class Key {
 
 }
 
-
 async function encryptDoc(doc) {
   if (doc === undefined) {
     throw new Error('doc must be defined')
   }
-
   const bytes = str2ab(JSON.stringify(doc))
   const { cipherbytes, iv } = await this.encrypt(bytes)
-
   return { _id:`entry-${iv.join('')}`, cipherbytes, iv }
 }
+
 async function decryptDoc(encDoc) {
   if (encDoc === undefined) {
     throw new Error('encDoc must be defined')
   }
-
   const decrypted = await this.decrypt(encDoc.cipherbytes, encDoc.iv)
   const clearObj = JSON.parse(ab2str(decrypted))
-
   return { internal:clearObj, external:encDoc }
 }
 
